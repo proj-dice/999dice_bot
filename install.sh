8cd9db8ed5434e1c2849d2ab13b0950d416b54aa
proj-dice <62695325+proj-dice@users.noreply.github.com>
2020-03-26 16:52:42 +0700

    Package python2 if any eror

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg install python2 -y
+pkg install curl -y
+pkg install termux-api -y
+pkg update
+pip2 install ssl
+pip2 install mime
+pip2 install colorama
+pip2 install email
+pip2 install bs4
+pip2 install requests
+pip2 install smtplib
